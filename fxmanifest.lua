2188145a063b71e1367459476aa2687976d587a1
kitotake <ahmed12199725@gmail.com>
2026-04-29 19:07:30 +0200

    feat: FX manifeste Et fichier

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'kitoake'
+name 'kt_hud'
+description 'Modular interaction system hud'
+version '1.0.0'
+
+shared_scripts {
+    'shared/config.lua',
+}
+
+client_scripts {
+    'client/main.lua',
+}
+
+server_scripts {
+     'server.lua'
+}
